292a559367d5812d478b08ec29582253263396d8
Md Quashar <mdquashar1412@gmail.com>
2025-08-11 21:00:48 +0530

    favicon and backend cors updated

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(bodyParser.json())
 app.use(cors({
-    origin: ['http://localhost:5173']
+    origin: ['http://localhost:5173','https://url-shortener-v42j.onrender.com']
 }))
 
 
