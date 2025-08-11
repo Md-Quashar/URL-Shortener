164ac9785320f1ff55ce1a4638b7b89a940c2aad
Md Quashar <mdquashar1412@gmail.com>
2025-08-11 21:37:28 +0530

    local storage removal after logout

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -6,18 +6,19 @@ function Header() {
   const navigate = useNavigate();
   const navToLogin = () => {
       localStorage.removeItem('authToken');
+      localStorage.removeItem('userId')
       navigate('/login'); 
   }
   const token= localStorage.getItem('authToken');
   const navToSignin=()=>{
-      navigate('/signin'); 
+      navigate('/signup'); 
   }
   return (
     <div className="header"> 
       <h1 className='header-text'>URL Shortener</h1>
       <div className='user'>
           <img src="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp" alt="user" className='user-icon' />
-          {token ? <div className="header-links" onClick={navToLogin}>Logout</div>:<div className='signin-links' onClick={navToSignin}>Sign In</div> }
+          {token ? <div className="header-links" onClick={navToLogin}>Logout</div>:<div className='signin-links' onClick={navToSignin}>Sign Up</div> }
           
       </div>
       
